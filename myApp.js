961529b66e5ea226a4adff60a9bf9a3bb7c8fae4
Lucas Guerra Cardoso <49378277+FooOperator@users.noreply.github.com>
2022-02-07 15:44:14 +0000

    Reached lesson 7 out of 12

--- myApp.js
+++ myApp.js
@@ -1,5 +1,33 @@
 var express = require('express');
+var path = require('path')
 var app = express();
+var http = require('http')
+
+const pathToIndexHTML = path.resolve(__dirname, 'views', 'index.html')
+
+function logger_get(req, res, next) {
+  console.log(`${req.method} ${req.path} - ${req.ip}`)
+  next()
+}
+
+app.use(express.static(path.resolve(__dirname, 'public')))
+
+app.use('/public', express.static(path.resolve(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+  res.sendFile(pathToIndexHTML)
+})
+
+app.use('/json', logger_get)
+
+app.get('/json', (req, res) => {
+  const messageValue = "Hello json"
+  const data = { "message": process.env['MESSAGE_STYLE'] === 'uppercase' ? messageValue.toUpperCase() : messageValue }
+  res.json(data)
+})
+
+
+const mySecret = process.env['MESSAGE_STYLE']
 
 
 
@@ -28,12 +56,4 @@ var app = express();
 
 
 
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
